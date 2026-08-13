ec7bd80fdfd551350ab82b224821a369053c49ee
ALGOTOP <zainmalikdps@gmail.com>
2026-08-13 06:04:16 +0500

    Create SmoothScroll.tsx

---
+++ components/SmoothScroll.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+export default function SmoothScroll() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+      syncTouch: false,
+      wheelMultiplier: 1,
+      touchMultiplier: 1.5,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    const animationFrame = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(animationFrame);
+      lenis.destroy();
+    };
+  }, []);
+
+  return null;
+}
